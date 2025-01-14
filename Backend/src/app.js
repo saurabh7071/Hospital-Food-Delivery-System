af6cb90b0c09a0c2538b4b4b2bef6575b85c08f5
Saurabh Vaidya <vaidyasaurabh48@gmail.com>
2025-01-14 12:18:10 +0530

    write meal delivery contoller and tested successfully on postman

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,6 +21,7 @@ import dietPlan from "./routes/dietPlan.route.js"
 import pantryStaff from "./routes/pantry-staff.route.js"
 import mealPreparation from "./routes/mealPreparation.route.js"
 import deliveryPerson from "./routes/delivery-person.route.js"
+import mealDelivery from "./routes/mealDelivery.route.js"
 
 // route declaration
 app.use("/api/v1/patient-details", patientDetails)
@@ -28,5 +29,6 @@ app.use("/api/v1/diet-plan", dietPlan)
 app.use("/api/v1/pantry-staff", pantryStaff)
 app.use("/api/v1/meal-preparation", mealPreparation)
 app.use("/api/v1/delivery-person", deliveryPerson)
+app.use("/api/v1/meal-delivery", mealDelivery)
 
 export {app}
